d5d7f725c5bf55e4e45075c8e83f51068e17333f
GluterusMaximus <supkar1756@gmail.com>
2022-08-29 17:57:21 +0300

    Removed comments

--- src/middlewares/errorMiddleware.js
+++ src/middlewares/errorMiddleware.js
@@ -1,20 +1,12 @@
 import ApiError from '../errors/ApiError.js';
 
-// eslint-disable-next-line no-unused-vars
-export default function (err, _req, res, _next) {
+export default function (err, _req, res, next) {
   console.error(err);
 
   if (err instanceof ApiError) {
     res.status(err.status).end();
-    // .json({
-    //   message: err.message,
-    //   errors: err.errors,
-    // });
   }
 
   res.status(500).end();
-  // .json({
-  //   message: 'An unknown error occurred',
-  //   errors: [],
-  // });
+  next();
 }
