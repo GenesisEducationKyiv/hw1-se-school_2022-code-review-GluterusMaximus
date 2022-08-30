1f983b61db09e340eee05eba2bf5a21b3847aa2d
GluterusMaximus <supkar1756@gmail.com>
2022-08-30 08:38:42 +0300

    Separated database and sending logic

--- src/controllers/SubscriptionController.js
+++ src/controllers/SubscriptionController.js
@@ -1,9 +1,11 @@
 import ApiError from '../errors/ApiError.js';
-import SubscriptionService from '../services/SubscriptionService.js';
+import DatabaseService from '../services/DatabaseService.js';
+import SendService from '../services/SendService.js';
 import { validateEmail } from '../utils/validators.js';
 
 class SubscriptionController {
-  #subService = new SubscriptionService();
+  #sendService = new SendService();
+  #databaseService = new DatabaseService();
 
   async subscribe(req, res, next) {
     try {
@@ -12,7 +14,7 @@ class SubscriptionController {
       if (!email || !validateEmail(email))
         throw new ApiError(400, 'Invalid email');
 
-      await this.#subService.subscribe(email);
+      await this.#databaseService.subscribe(email);
       res.status(200).end();
     } catch (error) {
       next(error);
@@ -21,7 +23,7 @@ class SubscriptionController {
 
   async sendEmails(_req, res, next) {
     try {
-      const notSent = await this.#subService.sendEmails();
+      const notSent = await this.#sendService.sendEmails();
       res.status(200).json({ notSent });
     } catch (error) {
       next(error);
